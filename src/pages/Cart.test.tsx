5ba41654b9d4fe065dcd5f7598f31a2b5a3e83b0
davidsilva <davidavilasilva@gmail.com>
2024-02-26 11:50:14 -0800

    Begin creating Cart and tests

--- src/pages/Cart.test.tsx
+++ src/pages/Cart.test.tsx
@@ -1,5 +1,97 @@
+import { CartContextProvider } from "../context/CartContext";
+import { Cart } from "./";
+import { CartItem } from "../context/CartContext";
+import { render } from "@testing-library/react";
+
+const { mockCartItems } = vi.hoisted(() => {
+  return {
+    mockCartItems: [
+      {
+        id: "1",
+        name: "Product 1",
+        description: "Description 1",
+        price: 100,
+        createdAt: "2021-09-01T00:00:00.000Z",
+        updatedAt: "2021-09-01T00:00:00.000Z",
+        __typename: "Product",
+        quantity: 2,
+      },
+      {
+        id: "2",
+        name: "Product 2",
+        description: "Description 2",
+        price: 200,
+        createdAt: "2021-09-01T00:00:00.000Z",
+        updatedAt: "2021-09-01T00:00:00.000Z",
+        __typename: "Product",
+        quantity: 3,
+      },
+      {
+        id: "3",
+        name: "Product 3",
+        description: "Description 3",
+        price: 300,
+        createdAt: "2021-09-01T00:00:00.000Z",
+        updatedAt: "2021-09-01T00:00:00.000Z",
+        __typename: "Product",
+        quantity: 4,
+      },
+    ] as CartItem[],
+  };
+});
+
+vi.mock("aws-amplify/auth");
+
+const { useCartContextMock } = vi.hoisted(() => {
+  return {
+    useCartContextMock: vi.fn().mockReturnValue({
+      cartItems: mockCartItems,
+      addToCart: vi.fn(),
+      removeFromCart: vi.fn(),
+      totalAmount: 2000,
+      clearCart: vi.fn(),
+      incrementQuantity: vi.fn(),
+      decrementQuantity: vi.fn(),
+    }),
+  };
+});
+
+vi.mock("../context/CartContext", async () => {
+  const actual = await import("../context/CartContext");
+  return {
+    ...actual,
+    useCartContext: useCartContextMock,
+  };
+});
+
+const { mockNavigate } = vi.hoisted(() => {
+  return { mockNavigate: vi.fn() };
+});
+
+vi.mock("react-router-dom", async () => {
+  const router = await vi.importActual<typeof import("react-router-dom")>(
+    "react-router-dom"
+  );
+  return {
+    ...router,
+    useNavigate: vi.fn().mockReturnValue(mockNavigate),
+  };
+});
+
+const renderComponent = () => {
+  render(
+    <CartContextProvider>
+      <Cart />
+    </CartContextProvider>
+  );
+};
+
 describe("cart", () => {
-  test.todo("should add product to cart");
+  beforeEach(() => {
+    vi.clearAllMocks();
+
+    renderComponent();
+  });
   test.todo("should remove product from cart");
   test.todo("should remove product from cart when quantity is 0");
   test.todo("should increment product quantity");
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,80 @@
+import { useCartContext } from "../context/CartContext";
+import { CartItem } from "../context/CartContext";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const {
+    cartItems,
+    removeFromCart,
+    clearCart,
+    incrementQuantity,
+    decrementQuantity,
+    totalAmount,
+  } = useCartContext();
+
+  const handleIncrementQuantity = (item: CartItem) => {
+    console.log("increment", item);
+    incrementQuantity(item);
+  };
+
+  const HandleDecrementQuantity = (item: CartItem) => {
+    console.log("decrement", item);
+    decrementQuantity(item);
+  };
+
+  const handleRemoveItem = (item: CartItem) => {
+    console.log("remove", item);
+    removeFromCart(item);
+  };
+
+  const handleCheckout = () => {
+    console.log("checkout");
+  };
+
+  const handleClearCart = () => {
+    console.log("clear");
+    clearCart();
+  };
+
+  return (
+    <>
+      <h1>Cart</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+              <td>{item.quantity}</td>
+              <td>{item.price * item.quantity}</td>
+              <td>
+                <Button onClick={() => handleIncrementQuantity(item)}>+</Button>
+                <Button onClick={() => HandleDecrementQuantity(item)}>-</Button>
+                <Button onClick={() => handleRemoveItem(item)}>X</Button>
+              </td>
+            </tr>
+          ))}
+          <tr>
+            <td colSpan={3}>Total</td>
+            <td>{totalAmount}</td>
+            <td>
+              <Button onClick={() => handleClearCart()}>Clear</Button>
+              <Button onClick={() => handleCheckout()}>Checkout</Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 };
 export default Cart;
