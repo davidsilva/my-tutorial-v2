5314f123d323113d5f08d199202f8aa1d02af6f1
davidsilva <davidavilasilva@gmail.com>
2024-04-11 16:21:12 -0700

    Use Erik's great idea for an async process type.

--- src/types.ts
+++ src/types.ts
@@ -11,8 +11,45 @@ export type ListProductsQueryWithReviews = {
   };
 };
 
+export enum AsyncProcessStatus {
+  NONE = "NONE",
+  PENDING = "PENDING",
+  ERROR = "ERROR",
+  SUCCESS = "SUCCESS",
+}
+
+export interface AsyncProcessBase {
+  status: AsyncProcessStatus;
+}
+
+export interface AsyncProcessNone extends AsyncProcessBase {
+  status: AsyncProcessStatus.NONE;
+}
+
+export interface AsyncProcessPending extends AsyncProcessBase {
+  status: AsyncProcessStatus.PENDING;
+}
+
+export interface AsyncProcessError<E> extends AsyncProcessBase {
+  status: AsyncProcessStatus.ERROR;
+  error: E;
+}
+
+export interface AsyncProcessSuccess<T> extends AsyncProcessBase {
+  status: AsyncProcessStatus.SUCCESS;
+  value: T;
+}
+
+export type AsyncProcess<T, E> =
+  | AsyncProcessNone
+  | AsyncProcessPending
+  | AsyncProcessError<E>
+  | AsyncProcessSuccess<T>;
+
 export type UserWithReviews = User & {
   reviews?: {
     items: Review[];
   };
 };
+
+export type Status = "loading" | "error" | "success";
